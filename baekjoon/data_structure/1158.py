0478cb06cbdb596cc9f015eaeb2fe40c0821cb3b
chungjm <chungjm0711@gmail.com>
2022-01-05 00:47:15 +0900

    git ALG commit

---
+++ baekjoon/data_structure/1158.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+N , K = map(int, input().split())
+
+arr = [i + 1 for i in range(N)]
+answer = [ ]
+index = 0
+
+for i in range(N):
+    index += K - 1;
+    if index >= len(arr):
+        index %= len(arr)
+    answer.append(str(arr.pop(index)))
+
+print('<', ', '.join(answer), '>', sep = '')
