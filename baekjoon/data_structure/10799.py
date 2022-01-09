4e0f36bfee4ff6214b9e144bf4aea2a983d3411e
Jongmin <chungjm0711@gmail.com>
2022-01-09 18:04:52 +0900

    git ALG commit

--- baekjoon/data_structure/10799.py
+++ baekjoon/data_structure/10799.py
@@ -1,8 +1,6 @@
 # https://www.acmicpc.net/problem/10799
-# 1. stack을 이요한 문제풀이
-# 2. stack을 대신하는 pipe_stack 변수
-
-# 1
+# stack을 이요한 문제풀이
+# stack을 대신하는 pipe_stack 변수
 from os import pipe
 import sys
 
@@ -18,15 +16,13 @@ while i < len(parentheses):
     x = parentheses[i]
     # '('가 막대의 시작인지, 레이저의 시작인지 구분 
     if x == '(':
-        # 레이저
-        if parentheses[i+1] == ')':    
+        if parentheses[i+1] == ')':    # 레이저
             res += pipe_stack
             i += 1
-        else:
-            # 새로운 막대 추가
+        else: # 막대
             res += 1
             pipe_stack += 1
-    else:
+    else: 
         pipe_stack -= 1
     i += 1
 
