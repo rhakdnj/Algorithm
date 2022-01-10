769103296444c5d17baedef6de1d7db01db4fbcb
Jongmin <chungjm0711@gmail.com>
2022-01-10 16:29:57 +0900

    git ALG commit

---
+++ baekjoon/math/5618.py
@@ -0,0 +1,30 @@
+import sys
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+def gcd(x, y):
+    if y == 0:
+        return x
+    else:
+        return gcd(y, x % y)
+
+n = int(input())
+arr = list(map(int, input().split()))
+res = []
+cd = arr[0]
+
+for i in range(n):
+    cd = gcd(cd, arr[i])
+
+x = 1
+while x * x <= gcd:
+    if gcd % x == 0:
+        res.append(x)
+        if x * x != gcd:
+            res.append(gcd // x)
+    x += 1
+
+res.sort()
+print(*res)
+
