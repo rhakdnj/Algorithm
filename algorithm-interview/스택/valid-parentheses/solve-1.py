b0c591a76b00320705811f5079cbe88a3cff02ed
Jongmin <chungjm0711@gmail.com>
2022-01-22 23:37:26 +0900

    git ALG commit

---
+++ algorithm-interview/스택/valid-parentheses/solve-1.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        table = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        }
+        # 스택 이용 예외 처리 및 일치 여부 판별
+        for char in s:
+            if char not in table:
+                stack.append(char)
+            elif not stack or table[char] != stack.pop():
+                return False
+
+        return len(stack) == 0
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    s = "()[]{}"
+    print(solution.isValid(s))
+
+
