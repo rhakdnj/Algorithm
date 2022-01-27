8d941e2161311a87e478643948c8dc19e50289ff
Jongmin <chungjm0711@gmail.com>
2022-01-27 16:37:04 +0900

    git ALG commit

---
+++ algorithm-interview/그래프/reconstruct-itinerary/solve-1.py
@@ -0,0 +1,26 @@
+from typing import *
+from collections import defaultdict
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+
+        graph = defaultdict(list)
+        for a, b in sorted(tickets):
+            graph[a].append(b)
+
+        route = []
+
+        def dfs(a):
+            # 첫 번째 값을 읽어 어휘 순으로 방문
+            while graph[a]:
+                dfs(graph[a].pop(0))
+            route.append(a)
+
+        dfs('JFK')
+        return route[::-1]
+
+
+solution = Solution()
+tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+solution.findItinerary(tickets)
