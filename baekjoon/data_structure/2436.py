0fb3c7f6084b0486fd2924c5d345abd79de022a7
Jongmin <chungjm0711@gmail.com>
2022-01-29 23:35:39 +0900

    git ALG commit

---
+++ baekjoon/data_structure/2436.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/2346
+from collections import deque
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+n = int(input())
+queue = deque(map(int, input().split()))
+queue2 = deque(i for i in range(1, n + 1))
+
+while queue:
+    q = queue[0]
+    if q > 0:
+        queue.popleft()
+        queue.rotate(-q + 1)
+        print(queue2.popleft())
+        queue2.rotate(-q + 1)
+    else:
+        queue.popleft()
+        queue.rotate(-q)
+        print(queue.popleft())
+        queue2.rotate(-q)
