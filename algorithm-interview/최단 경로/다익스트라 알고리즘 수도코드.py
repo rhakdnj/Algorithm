dd1184ef340184281cd49126fd2841cc91faa163
Jongmin <chungjm0711@gmail.com>
2022-02-13 23:20:56 +0900

    git ALG commit

---
+++ algorithm-interview/최단 경로/다익스트라 알고리즘 수도코드.py
@@ -0,0 +1,29 @@
+"""
+function Dijkstra(graph, source):
+    dist[source] <- 0
+
+    create vertex priority queue Q
+
+    for each vertex v in graph:
+        if v != source:
+            dis[v] <- INF
+            prev[v] <- UNDEFINED
+
+        Q.add_with_priority(v, dist[v])
+
+    while Q is not empty:
+        u <- Q.extract_min()
+        for each neighbor v of u:
+            alt <- dist[u] + length(u, v)
+            if alt < dist[v]:
+                dist[v] <- alt
+                prev[v] <- u
+                Q.decrease_priority(v, alt)
+
+    return dist, prev
+"""
+
+graph = dict()
+times = [[2, 1, 1]]
+for u, v, w in times:
+    graph[u].append((v, w))
