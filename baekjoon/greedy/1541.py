2275d7be7998d1493a70af5d28d1623b6858afd7
Jongmin <chungjm0711@gmail.com>
2022-02-13 22:18:17 +0900

    git Algo commit

--- baekjoon/greedy/1541.py
+++ baekjoon/greedy/1541.py
@@ -9,22 +9,10 @@ def input():
 s = list(input().split('-'))
 res = 0
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 for i in s[0].split('+'):
     res += int(i)
 for i in s[1:]:
     for j in i.split('+'):
         res -= int(j)
 
+print(res)
