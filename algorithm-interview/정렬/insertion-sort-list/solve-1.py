6b0de842782fdf657d2fb644d42ceee4f7301e0c
Jongmin <chungjm0711@gmail.com>
2022-02-16 22:54:28 +0900

    git ALG commit

---
+++ algorithm-interview/정렬/insertion-sort-list/solve-1.py
@@ -0,0 +1,21 @@
+from typing import *
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        cur = parent = ListNode(None)
+        while head:
+            while cur.next and cur.next.val < head.val:
+                cur = cur.next
+
+            cur.next, head.next, head = head, cur.next, head.next
+
+            cur = parent
+        return cur.next
