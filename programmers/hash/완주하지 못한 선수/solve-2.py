7364166c99ca4b761b9b1f2f4c32142d8589afbf
Jongmin <chungjm0711@gmail.com>
2022-02-17 13:49:49 +0900

    git ALG commit

---
+++ programmers/hash/완주하지 못한 선수/solve-2.py
@@ -0,0 +1,9 @@
+def solution(participant, completion):
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    return dic[temp]
