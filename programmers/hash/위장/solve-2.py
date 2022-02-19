63ad65a23252b42b07fc32b9fc8458e8ac6ac890
Jongmin <chungjm0711@gmail.com>
2022-02-20 01:04:59 +0900

    git ALG commit

--- programmers/hash/위장/solve-2.py
+++ programmers/hash/위장/solve-2.py
@@ -3,4 +3,4 @@ from functools import reduce
 
 
 def solution(c):
-    return reduce(lambda x,y:x*y,[a+1 for a in collections.Counter([x[1] for x in c]).values()])-1
+    return reduce(lambda x, y: x*y, [a+1 for a in collections.Counter([x[1] for x in c]).values()])-1
--- programmers/hash/위장/solve.py
+++ programmers/hash/위장/solve.py
@@ -9,7 +9,7 @@ def solution(clothes):
 
     counter = Counter(category)
 
-    #옷을 고르는 경우 + 옷을 입지 않는 경우 -> cnt + 1
+    # 옷을 고르는 경우 + 옷을 입지 않는 경우 -> cnt + 1
     for cnt in counter.values():
         answer *= cnt + 1
     else:
