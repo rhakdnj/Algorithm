df88068a6b466d9a505872b1f4fb5796efe9b991
Jongmin <chungjm0711@gmail.com>
2022-02-21 01:55:26 +0900

    git ALG commit

--- algorithm-interview/이진 검색/two-sum-ii-input-array-is-sorted/solve-2.py
+++ algorithm-interview/이진 검색/two-sum-ii-input-array-is-sorted/solve-2.py
@@ -3,3 +3,16 @@ from typing import *
 
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for k, v in enumerate(numbers):
+            left, right = k + 1, len(numbers) - 1
+            expected = target - v
+
+            # determining remainder values by binary search
+            while left <= right:
+                mid = left + (right - left) // 2
+                if numbers[mid] < expected:
+                    left = mid + 1
+                elif numbers[mid] > expected:
+                    right = mid - 1
+                else:
+                    return [k + 1, mid + 1]
---
+++ algorithm-interview/이진 검색/two-sum-ii-input-array-is-sorted/solve-4.py
@@ -0,0 +1,10 @@
+import bisect
+from typing import *
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        for k, v in enumerate(numbers):
+            expected = target - v
+            nums = numbers[k + 1:]
+            i = bisect.bisect_left(nums, expected)
