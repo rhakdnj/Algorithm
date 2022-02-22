3afa2ed0bdd4136822ee277db6ad1c0e3b01ef07
Jongmin <chungjm0711@gmail.com>
2022-02-22 15:32:02 +0900

    git ALG commit

---
+++ algorithm-interview/그리디 알고리즘/assign-cookies/solve-2.py
@@ -0,0 +1,16 @@
+import bisect
+from typing import *
+
+
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        result = 0
+        for i in s:
+            index = bisect.bisect_right(g, i)
+            if index > result:
+                result += 1
+
+        return result
