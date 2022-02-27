b552847b1306fda709f28690c17a84bd49045dfd
Jongmin <chungjm0711@gmail.com>
2022-02-27 18:53:05 +0900

    git alg commit

--- programmers/sort/가장 큰 수/solve.py
+++ programmers/sort/가장 큰 수/solve.py
@@ -1,4 +1,6 @@
 def solution(numbers):
+    if sum(numbers) == 0: return '0'
     numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    return str(int(''.join(numbers)))
+    numbers.sort(key=lambda x: x * 3, reverse=True)
+    answer = "".join(numbers)
+    return answer
