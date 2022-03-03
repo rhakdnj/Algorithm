17bf6b919b1e7ce1f8994cd5cf04b6e84ee26aca
Jongmin <chungjm0711@gmail.com>
2022-03-04 08:41:37 +0900

    git alg commit

--- baekjoon/greedy/20365.py
+++ baekjoon/greedy/20365.py
@@ -1,1 +1,10 @@
-#https://www.acmicpc.net/problem/20365
+# https://www.acmicpc.net/problem/20365
+N = int(input())
+colors = input()
+cnt = {'B': 0, 'R': 0}
+cnt[colors[0]] += 1
+
+for i in range(1, N):
+    if colors[i] != colors[i - 1]:
+        cnt[colors[i]] += 1
+print(min(cnt['B'], cnt['R']) + 1)
