fe1d699a1dab87be9ddd7ac09f9267216aaf2941
Jongmin <chungjm0711@gmail.com>
2022-03-13 20:39:24 +0900

    git alg commit

---
+++ this-is-coding-test/dfs_bfs/미로 탈출/solve.py
@@ -0,0 +1,35 @@
+'''
+시작 위치 : (1, 1)
+미로의 출구 : (N, M)
+한 번에 한 칸씩 이동할 수 있다
+이 때 괴물이 있는 부분을 0 으로, 괴물이 없는 부분을 1로 표시 되어 있어
+'''
+from collections import deque
+
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0 or nx >= n or ny <0 or ny >= m:
+                continue
+            if graph[nx][ny] == 0:
+                continue
+            if graph[nx][ny]:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx, ny))
+
+    return graph[n-1][m-1]
+
+
+print(bfs(0, 0))
--- this-is-coding-test/dfs_bfs/음료수 얼려 먹기/solve.py
+++ this-is-coding-test/dfs_bfs/음료수 얼려 먹기/solve.py
@@ -1,26 +1,24 @@
-import sys
-
-
-def input():
-    return sys.stdin.readline().rstrip()
+n, m = map(int, input().split())
+graph = [list(map(int, input())) for _ in range(n)]
 
 
 def dfs(x, y):
-    if x < 0 or x > len(graph[0]) or y > len(graph) or y < 0:
+    if x < 0 or x >= len(graph) or y < 0 or y >= len(graph):
         return False
-    else:
-        dfs(x + 1, y)
+    if graph[x][y] == 0:
+        graph[x][y] = 1
+        # 상, 하, 좌, 우
         dfs(x - 1, y)
+        dfs(x + 1, y)
         dfs(x, y - 1)
         dfs(x, y + 1)
+        return True
+    return False
 
 
-n, m = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(n)]
 answer = 0
 for i in range(n):
     for j in range(m):
         if dfs(i, j):
             answer += 1
-
 print(answer)
