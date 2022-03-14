5824949d3b009124d3005ac08cd4d56aafe6d5d5
Jongmin <chungjm0711@gmail.com>
2022-03-14 12:11:21 +0900

    git alg commit

--- this-is-coding-test/dfs_bfs/미로 탈출/solve.py
+++ this-is-coding-test/dfs_bfs/미로 탈출/solve.py
@@ -21,7 +21,7 @@ def bfs(x, y):
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if nx < 0 or nx >= n or ny <0 or ny >= m:
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
             if graph[nx][ny] == 0:
                 continue
