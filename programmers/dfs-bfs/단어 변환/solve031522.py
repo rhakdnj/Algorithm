e6a22d499c5c0752cecbc691df280e238f2d341c
Jongmin <chungjm0711@gmail.com>
2022-03-15 21:37:31 +0900

    git alg commit

---
+++ programmers/dfs-bfs/단어 변환/solve031522.py
@@ -0,0 +1,12 @@
+def dfs(numbers, target, depth):
+    answer = 0
+    if depth == len(numbers):
+        if sum(numbers) == target:
+            return 1
+        else:
+            return 0
+    else:
+        answer += dfs(numbers, target, depth + 1)
+        numbers[depth] *= (-1)
+        answer += dfs(numbers, target, depth + 1)
+        return answer
