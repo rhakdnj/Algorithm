e9bec5dc14fb14f2b8a2df04bcc1750dde5c50e7
Jongmin <chungjm0711@gmail.com>
2022-03-28 21:31:57 +0900

    git alg commit

---
+++ this-is-coding-test/search/binary-search/binary-search-repetitive.py
@@ -0,0 +1,32 @@
+'''
+이진 탐색은 배열 내부의 데이터가 정렬되어 있어야만 사용할 수 있는 알고리즘이다.
+이진 탐색은 탐색 범위를 절반씩 좁혀가며 데이터를 탐색하는 특징이 있다.
+
+이진 탐색은 위치를 나타내는 변수 3개를 사용하는데 탐색하고자 하는 범위의 시작점, 끝점, 그리고 중간점이다.
+찾으려는 데이터와 중간점 위치에 있는 데이터를 반복적으로 비교해서 원하는 데이터를 찾는게 이진 탐색 과정이다.
+'''
+
+
+# 이진 탐색 소스코드 구현(재귀함수)
+def binary_search(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+n, target = map(int, input().split())
+# 전체 원소 입력받기
+array = list(map(int, input().split()))
+
+# 이진 탐색 수행 결과 출력
+result = binary_search(array, target, 0, n - 1)
+if result == None:
+    print("원소가 존재하지 않습니다.")
+else:
+    print(result + 1)
