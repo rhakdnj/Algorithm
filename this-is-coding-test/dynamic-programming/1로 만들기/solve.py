c6280d1e09e2d868da1b31c4a5a8b6106fd4866c
Jongmin <chungjm0711@gmail.com>
2022-03-30 17:02:55 +0900

    git dp notion commit

---
+++ this-is-coding-test/dynamic-programming/1로 만들기/solve.py
@@ -0,0 +1,35 @@
+"""
+input : x
+
+정수 x가 주어졌을 때, 연산 4개를 적절히 사용해서 1을 만드려고 한다.
+
+연산 4개:
+1. x가 5로 나누어떨어지면 5로 나눈다.
+2. x가 3으로 나누어떨어지면 3으로 나눈다.
+3. x가 2으로 나누어떨어지면 2으로 나눈다.
+4. x에서 1을 뺀다.
+"""
+
+import sys
+
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+x = int(input())
+dp = [0] * 30001
+
+for i in range(2, x + 1):
+    dp[i] = dp[i - 1] + 1
+
+    if i % 2 == 0:
+        dp[i] = min(dp[i], dp[i // 2] + 1)
+
+    if i % 3 == 0:
+        dp[i] = min(dp[i], dp[i // 3] + 1)
+
+    if i % 5 == 0:
+        dp[i] = min(dp[i], dp[i // 5] + 1)
+
+print(dp[x])
