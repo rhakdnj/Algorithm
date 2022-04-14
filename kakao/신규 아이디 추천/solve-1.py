e17ef31c0be208983528aa39c527f07c4be262a7
Jongmin <chungjm0711@gmail.com>
2022-04-14 18:53:32 +0900

    change directory

--- kakao/신규 아이디 추천/solve-1.py
+++ kakao/신규 아이디 추천/solve-1.py
@@ -5,7 +5,7 @@ def solution(new_id):
     st = new_id
     st = st.lower()
     st = re.sub('[^a-z0-9\-_.]', '', st)
-    st = re.sub('\.+', '.', st)
+    st = re.sub('\.+', '', st)
     st = re.sub('^[.]|[.]$', '', st)
     st = 'a' if len(st) == 0 else st[:15]
     st = re.sub('^[.]|[.]$', '', st)
