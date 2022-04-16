42558ec09d82ef75830f4e0f881509887848db9d
Jongmin <chungjm0711@gmail.com>
2022-04-16 14:08:33 +0900

    update: 튜플

--- kakao/튜플/solve-1.py
+++ kakao/튜플/solve-1.py
@@ -2,6 +2,3 @@ def solution(s):
     s = eval(s.replace("{", "[").replace("}", "]"))
     answer = list({num: 0 for k in sorted(s, key=lambda x: len(x)) for num in k}.keys())
     return answer
-
-
-print(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}"))
--- kakao/튜플/solve.py
+++ kakao/튜플/solve.py
@@ -1,27 +1,18 @@
 def solution(s):
     answer = []
-    set_list = []
-    stack = []
-    temp = ''
-    for i in range(1, len(s) - 1):
-        if s[i] == ',' and len(stack) == 0:
-            continue
-        if s[i] == '{':
-            stack.append('{')
-        elif s[i] == '}':
-            stack.pop()
-            set_list.append(temp)
-            temp = ''
-        else:
-            temp += s[i]
-    set_list.sort(key=lambda x: len(x))
-    answer.append(int(set_list[0]))
-    for i in range(1, len(set_list)):
-        temp = set_list[i].split(',')
-        for j in temp:
-            if int(j) in answer:
-                continue
-            else:
-                answer.append(int(j))
+
+    s1 = s.lstrip('{').rstrip('}').split('},{')
+
+    new_s = []
+    for i in s1:
+        # 문자 ','이 없어도 split 가능
+        new_s.append(i.split(','))
+
+    new_s.sort(key=len)
+
+    for i in new_s:
+        for j in range(len(i)):
+            if int(i[j]) not in answer:
+                answer.append(int(i[j]))
 
     return answer
