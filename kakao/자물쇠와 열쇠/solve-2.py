f69cf4d6cd64c3f16a3fbd0243e78b673708cab9
Jongmin <chungjm0711@gmail.com>
2022-04-18 12:25:56 +0900

    re-solve: 자물쇠와 열쇠

--- kakao/자물쇠와 열쇠/solve-2.py
+++ kakao/자물쇠와 열쇠/solve-2.py
@@ -1,10 +1,3 @@
-"""
-자물쇠를 고정 시키고 열쇠를 자물쇠의 크기만큼 순회를 해서 맞는지 확인하는 것
-따라서 offset을 생각하고 4가지 회전을 다 찾아보는 방법!
-브루트포스
-"""
-
-
 def match(arr, key, rot, r, c):
     n = len(key)
     for i in range(n):
@@ -28,17 +21,19 @@ def check(arr, offset, n):
 
 
 def solution(key, lock):
-    offset = len(key) - 1
+    m = len(key)
+    offset = m - 1
+    n = len(lock)
 
-    for r in range(offset + len(lock)):
-        for c in range(offset + len(lock)):
+    for r in range(offset + n):
+        for c in range(offset + n):
             for rot in range(4):
                 arr = [[0 for _ in range(58)] for _ in range(58)]
-                for i in range(len(lock)):
-                    for j in range(len(lock)):
+                for i in range(n):
+                    for j in range(n):
                         arr[offset + i][offset + j] = lock[i][j]
 
                 match(arr, key, rot, r, c)
-                if check(arr, offset, len(lock)):
+                if check(arr, offset, n):
                     return True
     return False
