a3f258cda92c6892fdbade7e10ba956a9d998cca
Jongmin <chungjm0711@gmail.com>
2022-04-22 17:55:57 +0900

    solve : kakao

---
+++ kakao/[3차] n진수 게임/solve-1.py
@@ -0,0 +1,11 @@
+def solution(n, t, m, p):
+    data = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+    numbers = "0"
+    for number in range(1, t * m):
+        temp = []
+        while number > 0:
+            temp.append(data[number % n])
+            number //= n
+        numbers += "".join(reversed(temp))
+
+    return numbers[p - 1:t * m:m]
