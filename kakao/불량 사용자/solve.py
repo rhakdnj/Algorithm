31ddf7027e5b10db2283a9dc63a7a0f9254f3b3a
Jongmin <chungjm0711@gmail.com>
2022-04-29 19:31:14 +0900

    solve: 불량 사용자

---
+++ kakao/불량 사용자/solve.py
@@ -0,0 +1,29 @@
+from itertools import permutations
+
+
+def check(users, banned_id):
+    for i in range(len(banned_id)):
+        if len(banned_id[i]) != len(users[i]):
+            return False
+        for j in range(len(users[i])):
+            if banned_id[i][j] == '*':
+                continue
+            elif banned_id[i][j] != users[i][j]:
+                return False
+    return True
+
+
+def solution(user_id, banned_id):
+    answer = []
+    p = permutations(user_id, len(banned_id))
+
+    for users in p:
+        if not check(users, banned_id):
+            continue
+        else:
+            users = set(users)
+            if users in answer:
+                continue
+            answer.append(users)
+
+    return len(answer)
