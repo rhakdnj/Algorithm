125a245054677ed4f02271f35080e26b4e949d34
James <74996516+chungjm@users.noreply.github.com>
2022-05-12 14:31:20 +0900

    solve: 백준 1753

--- baekjoon/shortest-path/1753.py
+++ baekjoon/shortest-path/1753.py
@@ -8,8 +8,6 @@ v, e = map(int, input().split())
 start = int(input())
 # 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트
 graph = [[] for _ in range(v + 1)]
-# 방문 여부 리스트
-visited = [False] * (v + 1)
 # 최단 거리 테이블을 모두 무한으로 초기화
 distance = [INF] * (v + 1)
 
@@ -23,19 +21,17 @@ def dijkstra(start):
     heap = []
     heappush(heap, (0, start))
     distance[start] = 0
-
     while heap:
         dist, curr = heappop(heap)
-        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+
         if distance[curr] < dist:
             continue
-        # 현재 노드와 연결된 다른 노드들을 확인
-        for i, dist in graph[curr]:
-            # 현재 노드를 거쳐서, 다른 노드로 이동하는 거리가 더 짧은 경우
-            cost = distance[curr] + dist
-            if cost < distance[i]:
-                distance[i] = cost
-                heappush(heap, (cost, i))
+
+        for i in graph[curr]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heappush(heap, (cost, i[0]))
 
 
 dijkstra(start)
