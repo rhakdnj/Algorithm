3f9338270c32571e1337325e81a1bb7dc5d8602e
jmjmjames <chungjm0711@gmail.com>
2022-07-19 15:36:24 +0900

    [20220719] :백준 테트로미노 dfs

--- baekjoon/graph_traversal/14500.py
+++ baekjoon/graph_traversal/14500.py
@@ -1,40 +1,43 @@
+# https://www.acmicpc.net/problem/14500
+# 테트로미노
+
 import sys
 
 sys.setrecursionlimit(5000)
 
-
-def input():
-    return sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
 
 
-def dfs(x, y, idx, total):
-    global answer
-    if answer >= total + max_val * (3 - idx):
+# dy : 4 | (dx, dy) = (0, 4), (1, 3), (2, 2)
+def dfs(x, y, depth, total):
+    global answer, max_val
+    # pruning
+    if answer >= total + max_val * (3 - depth):
         return
-    if idx == 3:
+    if depth == 3:
         answer = max(answer, total)
         return
-    else:
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-                if idx == 1:
-                    visited[nx][ny] = 1
-                    dfs(x, y, idx + 1, total + graph[nx][ny])
-                    visited[nx][ny] = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
+            # 블록 : (1, 3) ㅜ, ㅗ (3, 1) ㅓ ㅏ 처리
+            if depth == 1:
                 visited[nx][ny] = 1
-                dfs(nx, ny, idx + 1, total + graph[nx][ny])
+                dfs(x, y, depth + 1, total + graph[nx][ny])
                 visited[nx][ny] = 0
+            visited[nx][ny] = 1
+            dfs(nx, ny, depth + 1, total + graph[nx][ny])
+            visited[nx][ny] = 0
 
 
 n, m = map(int, input().split())
 graph = [list(map(int, input().split())) for _ in range(n)]
 visited = [[0] * m for _ in range(n)]
-dx, dy = [1, -1, 0, 0], [0, 0, 1, -1]
+dx, dy = (-1, 0, 1, 0), (0, 1, 0, -1)
 answer = 0
+# 2차원 배열의 최대 값 찾기
 max_val = max(map(max, graph))
-# 가로 4개, 가로 2 + 세로 2, 세로3 + 가로1,
 
 for i in range(n):
     for j in range(m):
