5e838f6b31d8ca751668746ff35893fb72749ea7
jmjmjames <chungjm0711@gmail.com>
2022-09-23 15:05:54 +0900

    [BOJ] Refactor: 17071 | 숨바꼭질 5 #46

--- baekjoon/17071/숨바꼭질 5.py
+++ baekjoon/17071/숨바꼭질 5.py
@@ -1,46 +1,53 @@
 from collections import deque
 
-n, k = map(int, input().split())
-MAX = int(5e5)
-visited = [[0 for _ in range(MAX + 1)] for _ in range(2)]
-
-
-def bfs():
-    global n, MAX, visited
-    dq = deque([(n, 0)])
-
-    visited[0][n] = 1
-    while len(dq):
-        now, cnt = dq.popleft()
-        flag = cnt % 2
-
-        for next_ in (now - 1, now + 1, now * 2):
-            if 0 <= next_ <= MAX and not visited[1 - flag][next_]:
-                visited[1 - flag][next_] = cnt + 1
-                dq.append((next_, cnt + 1))
+MAX_N = int(5e5)
 
 
 def solution():
-    global n, k, MAX, visited
+    n, k = map(int, input().split())
+    visited = [[0 for _ in range(MAX_N + 1)] for _ in range(2)]
+    turn, flag = 1, 0
 
     if n == k:
         print(0)
+        return
 
-    bfs()
+    dq = deque()
+    visited[0][n] = 1
+    dq.append(n)
+    while len(dq):
+        k += turn
 
-    t = 0
-    flag = 0
-    ret = -1
-    while k <= MAX:
-        if visited[flag][k]:
-            if visited[flag][k] <= t:
-                ret = t
+        if k > MAX_N:
+            break
+        if visited[turn % 2][k]:
+            flag = 1
+            break
+
+        for i in range(len(dq)):
+            now = dq.popleft()
+            for nxt in (now + 1, now - 1, now * 2):
+                if nxt < 0 or nxt > MAX_N or visited[turn % 2][nxt]:
+                    continue
+                visited[turn % 2][nxt] = visited[(turn + 1) % 2][now] + 1
+
+                if nxt == k:
+                    flag = 1
+                    break
+
+                dq.append(nxt)
+
+            if flag:
                 break
-        flag = 1 - flag
-        t += 1
-        k += t
+        if flag:
+            break
+        turn += 1
 
-    print(ret)
+    if flag:
+        print(turn)
+    else:
+        print(-1)
+    return
 
 
 solution()
