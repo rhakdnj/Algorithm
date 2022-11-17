f1fffc3f0065a50206268059e8b46709d5868a95
jmjmjames <chungjm0711@gmail.com>
2022-11-17 18:05:03 +0900

    [BOJ] 11003 | 최솟값 찾기

--- baekjoon/11003-최솟값 찾기.py
+++ baekjoon/11003-최솟값 찾기.py
@@ -1,13 +1,13 @@
 from collections import deque
+import sys
 
+input = lambda: sys.stdin.readline().rstrip()
 n, l = map(int, input().split())
-arr = list(map(int, input().split()))
-answer = []
 
 
 def solution():
     dq = deque()
-    for i, curr in enumerate(arr):
+    for i, curr in enumerate(list(map(int, input().split()))):
         while dq and dq[-1][1] > curr:
             dq.pop()
 
@@ -15,9 +15,7 @@ def solution():
         if dq[0][0] <= i - l:
             dq.popleft()
 
-        answer.append(dq[0][1])
-
-    print(f"{' '.join(map(str, answer))}")
+        print(dq[0][1], end=" ")
 
 
 solution()
